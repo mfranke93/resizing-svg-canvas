9b6e48db5553ca6367a6853a984ebecf4194f55e
Max Franke <Max.Franke@vis.uni-stuttgart.de>
2022-08-26 13:30:04 +0200

    v0.1.0: new API allowing for stacking elements, and HTMLCanvas
    The new version allows for more than one element in the same container. These
    elements may also be canvas elements now, and are stacked accordingly.

--- README.md
+++ README.md
@@ -1,21 +1,27 @@
-Create an SVG element inside a given parent element that resizes automatically.
+Create SVG and Canvas elements inside a given parent element that resizes automatically.
 
-SVG elements are explicitly sized (300&times;150px by default).
-However, for visualization purposes it is often desirable to have the SVG element resize extrinsically, for example as part of a CSS grid.
-In such situations, the SVG contents might need to be redrawn or rescaled.
+SVG and Canvas elements are explicitly sized (300&times;150px by default).
+However, for visualization purposes it is often desirable to have the element resize extrinsically, for example as part of a CSS grid.
+In such situations, the contents might need to be redrawn or rescaled.
 
-This library provides one function, `createResizingSVG`.
-This function takes one argument, the parent node within which the SVG element will reside.
+This library provides one class, `ResizeContainer`.
+This function takes one argument, the parent node within which the elements will reside.
 This parent node **must** be extrinsically or explicitly sized, either by setting `width` or `min-width`, and `height` or `min-height`, or, for example, by putting it into a grid.
-The function then uses a `ResizeObserver` to ensure that the SVG element's `width`, `height`, and `viewBox` are always updated.
-Calling `createResizingSVG` will *remove the contents of its argument HTML element!*
-It returns an object with three elements:
+The function then uses a `ResizeObserver` to ensure that the elements' `width`, `height`, and `viewBox` are always updated.
+Passing an element to the class constructor will *remove its contents!*
 
- - `svg`: the SVG element node itself
- - `parentNode`: the parent node
- - `initialWidth`: the current width of the SVG element
- - `initialHeight`: the current height of the SVG element
- - `cleanup`: a zero-argument function with no return value that clears the `ResizeObserver` and removes the contents of `parentNode`
+Multiple child elements can be added, one by the other.
+For an SVG element, call the `ResizeContainer` object's `addSvgLayer()` method, for a Canvas element, the `addCanvasLayer()` method.
+Both methods return the created element.
+Elements are stacked by `z-index` in the order of their creation (first created element is the furthest to the back).
+The order can be modified by calling the `sort()` function, which takes a comparison function as its argument which is passed to `Array.sort()`.
+Elements can be removed from the `ResizeContainer` by passing them to the `removeLayer()` function.
 
-Each time the parent element is resized, the SVG element is resized accordingly, and a `CustomEvent` of type `resize` is fired on the SVG element.
+Three additional read-only fields exist on the `ResizeContainer`:
+ - `container`: The HTML element of the container itself.
+ - `width`: The current width, in pixels, of the container.
+ - `height`: The current height, in pixels, of the container.
+
+Each time the parent element is resized, each child element is resized accordingly, and a `CustomEvent` of type `resize` is fired on each element.
 The event's `detail` field will contain an object with the new `width` and `height` values.
+An identical `CustomEvent` of type `resize` will be fired on the parent element itself.
--- src/index.ts
+++ src/index.ts
@@ -1,51 +1,100 @@
-interface ReturnValue {
-  svg: SVGSVGElement;
-  parentNode: HTMLElement;
+type ChildNode = SVGSVGElement | HTMLCanvasElement;
 
-  initialWidth: number;
-  initialHeight: number;
+export default class ResizeContainer {
+  private children: Array<ChildNode> = [];
+  private readonly observer: ResizeObserver;
 
-  cleanup: () => void;
+  private _cachedWidth: number = 0;
+  private _cachedHeight: number = 0;
+
+  constructor(
+    private readonly _container: HTMLElement,
+  ) {
+    this._container.innerHTML = '';
+    this._container.style.display = 'grid';
+    this._container.style.position = 'relative';
+    this._container.style.isolation = 'isolate';
+
+    this.observer = new ResizeObserver(( [{ contentBoxSize: [{ inlineSize, blockSize }]}] ) => this.handleResizeEvent(inlineSize, blockSize));
+    this.observer.observe(this._container);
+
+    const { width, height } = this._container.getBoundingClientRect();
+    this.handleResizeEvent(width, height);
+  }
+
+  private handleResizeEvent(width: number, height: number): void {
+    this._cachedWidth = width;
+    this._cachedHeight = height;
+
+    this.children.forEach(child => this.resizeChild(child));
+
+    this._container.dispatchEvent(
+      new CustomEvent('resize', {
+        detail: { width, height, },
+        bubbles: false,
+      }));
+  }
+
+  private resizeChild(child: ChildNode): void {
+    child.setAttribute('width', `${this._cachedWidth}`);
+    child.setAttribute('height', `${this._cachedHeight}`);
+    if (child instanceof SVGSVGElement) child.setAttribute('viewBox', `0 0 ${this._cachedWidth} ${this._cachedHeight}`);
+
+    child.dispatchEvent(
+      new CustomEvent('resize', {
+        detail: { width: this._cachedWidth, height: this._cachedHeight, },
+        bubbles: false,
+      }));
+  }
+
+  addSvgLayer(): SVGSVGElement {
+    const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+    svg.style.position = 'absolute';  // this is so that it does not affect the size of the wrapper element
+    svg.style.zIndex = `${this.children.length + 1}`;
+
+    this._container.appendChild(svg);
+    this.children.push(svg);
+    this.resizeChild(svg);
+
+    return svg;
+  }
+
+  addCanvasLayer(): HTMLCanvasElement {
+    const canvas = document.createElement('canvas');
+    canvas.style.gridArea = '1 / 1 / 2 / 2';
+    canvas.style.zIndex = `${this.children.length + 1}`;
+
+    this._container.appendChild(canvas);
+    this.children.push(canvas);
+    this.resizeChild(canvas);
+
+    return canvas;
+  }
+
+  removeLayer(layer: ChildNode): void {
+    const idx = this.children.indexOf(layer);
+
+    if (idx === -1) return console.warn('no such layer in ResizeContainer:', layer);
+
+    this.children = [ ...this.children.slice(0, idx), ...this.children.slice(idx + 1) ];
+    this._container.removeChild(layer);
+  }
+
+  sort(comp: ((a: ChildNode, b: ChildNode) => number)): void {
+    this.children
+      .sort(comp)
+      .forEach((child, index) => child.style.zIndex = (index + 1).toString());
+  }
+
+  get container(): HTMLElement {
+    return this._container;
+  }
+
+  get width(): number {
+    return this._cachedWidth;
+  }
+
+  get height(): number {
+    return this._cachedHeight;
+  }
 };
-
-export function createResizingSVG(parentNode: HTMLElement): ReturnValue {
-  parentNode.innerHTML = '';
-  const oldDisplay = parentNode.style.display;
-  const oldPosition = parentNode.style.position;
-  parentNode.style.display = 'grid';
-  parentNode.style.position = 'relative';
-
-  const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
-  svg.style.position = 'absolute';
-  parentNode.appendChild(svg);
-
-  const resize = (width: number, height: number) => {
-    svg.setAttribute('width', `${width}`);
-    svg.setAttribute('height', `${height}`);
-    svg.setAttribute('viewBox', `0 0 ${width} ${height}`);
-  };
-
-  const { width, height } = parentNode.getBoundingClientRect();
-  resize(width, height);
-
-  const obs = new ResizeObserver(([ { contentBoxSize: [{ inlineSize, blockSize }] } ]) => {
-    resize(inlineSize, blockSize);
-    svg.dispatchEvent(new CustomEvent('resize', { detail: { width: inlineSize, height: blockSize } }));
-  });
-
-  obs.observe(parentNode);
-  const cleanup = () => {
-    parentNode.innerHTML = '';
-    parentNode.style.display = oldDisplay;
-    parentNode.style.position = oldPosition;
-    obs.disconnect();
-  };
-
-  return {
-    svg,
-    parentNode,
-    cleanup,
-    initialWidth: width,
-    initialHeight: height,
-  };
-}
